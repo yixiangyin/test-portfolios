0e22463bad202de80d38bc6d8c04fb4eac0401ad
yixiangyin <y1027020239@qq.com>
2025-08-18 20:17:43 +1000

    update

--- components/about.tsx
+++ components/about.tsx
@@ -20,9 +20,8 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="font-medium">Computer Science</span>, I decided to pursue my
+        passion for programming. I am currently working as a part time software engineer at ANU.{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
@@ -30,19 +29,19 @@ export default function About() {
         <span className="font-medium">
           React, Next.js, Node.js, and MongoDB
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
+        . I am also familiar with Java, Python, C and TypeScript. I am always looking to
         learn new technologies. I am currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
         developer.
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        <span className="italic">When I'm not coding</span>, I enjoy listening to music, watching movies,{" "}
+        and reading. I always enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="font-medium">psychology</span>.{" "}
+        I'm also learning how to play the guitar.
       </p>
     </motion.section>
   );
--- lib/data.ts
+++ lib/data.ts
@@ -1,10 +1,11 @@
-import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import React from "react"
+import { CgWorkAlt } from "react-icons/cg"
+import { FaReact } from "react-icons/fa"
+import { LuGraduationCap } from "react-icons/lu"
+import rattleImg from "@/public/rattle.png"
+import dashboardImg from "@/public/dashboard.png"
+import phonebookImg from "@/public/phonebook.png"
+import wordanalyticsImg from "@/public/wordanalytics.png"
 
 export const links = [
   {
@@ -31,77 +32,114 @@ export const links = [
     name: "Contact",
     hash: "#contact",
   },
-] as const;
+] as const
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Graduated from ANU",
+    location: "Canberra, ACT",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "I graduated from ANU with a Bachelor of Advanced Computing (Honours).",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "Feb 2020 - Oct 2024",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "ANU Software Engineer Intern",
+    location: "Canberra, ACT",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "Served as a core developer in the modernization upgrade project (RattleNG) of Rattle, a widely used data science teaching tool with over 15 years of service to governments and universities (including ANU). Key contributions included developing text word cloud functionality, leading core widget design and development to enhance tool usability and stability and designing automated testing frameworks.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "Feb 2024 - Oct 2024",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "ANU Instructor",
+    location: "Canberra, ACT",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
+      "Courses: Algorithms and Data Structures, Python. Raising course satisfaction from 67% in 2022 to 92%, setting a new record for the course.",
+    icon: React.createElement(CgWorkAlt),
+    date: "July 2023 - Present",
   },
-] as const;
+
+  {
+    title: "ANU Software Engineer",
+    location: "Canberra, ACT",
+    description:
+      "Engineered the interactive data dashboard for the below zero emission project with python and plotly dash which guides ANU's policy on sustainability. Continued engineering the core UI widgets for Rattle V6 in Flutter - a data mining application widely used in industry and academia.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Aug 2023 - Present",
+  },
+] as const
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Rattle",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "Rattle The R Analytical Tool To Learn Easily is a popular GUI for data mining using R.",
+    tags: [
+      "Flutter",
+      "Dart",
+      "R",
+      "Data Science",
+      "UI/UX",
+      "Inter-process communication",
+    ],
+    imageUrl: rattleImg,
   },
   {
-    title: "rmtDev",
+    title: "Below Zero Emission",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      "Interactive dashboard for the Below Zero Emission project that guides ANU's policy on sustainability.",
+    tags: ["Python", "Plotly Dash", "Numpy", "Pandas", "Data Analytics"],
+    imageUrl: dashboardImg,
   },
   {
-    title: "Word Analytics",
+    title: "Phonebook",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "Full-stack contact book application with full CRUD operations, data validation, real-time updates, and robust error handling.",
+    tags: ["React", "Node", "MongoDB", "Vite", "REST API", "Express"],
+    imageUrl: phonebookImg,
   },
-] as const;
+] as const
 
 export const skillsData = [
-  "HTML5",
+  "HTML",
   "CSS",
   "JavaScript",
   "TypeScript",
+  "Bootstrap",
+  // "TypeScript",
   "React",
-  "Next.js",
+  // "Next.js",
   "Node.js",
-  "Git",
-  "Tailwind",
-  "Prisma",
-  "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
   "Express",
+  "Flutter",
+  "Dart",
+  "Git",
+  "Github",
+  "Machine Learning",
+  "UI/UX",
+  // "Tailwind",
+  // "Prisma",
+  "MongoDB",
+  // "Redux",
+  // "GraphQL",
+  // "Apollo",
   "PostgreSQL",
+  "SQLite",
+  "Java",
+  "C/C++",
   "Python",
-  "Django",
-  "Framer Motion",
-] as const;
+  "Numpy",
+  "Pandas",
+  "Flask",
+  "Plotly Dash",
+  "Cyber Security",
+  "Operating Systems",
+  "Computer Networks",
+  "Parallel Systems",
+  "High Performance Computing",
+  "Human-Computer Interaction",
+
+  // "Django",
+  // "Framer Motion",
+] as const
